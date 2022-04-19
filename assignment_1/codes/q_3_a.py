9510883d8c28b7e2aafc3a63706a35ffd4196548
D-Chandrahas <97086964+D-Chandrahas@users.noreply.github.com>
2022-04-19 13:35:23 +0530

    Add files via upload

--- assignment_1/codes/q_3_a.py
+++ assignment_1/codes/q_3_a.py
@@ -1,8 +1,8 @@
-from sympy import Symbol,simplify,sin,cos
+from sympy import pi,simplify,sin,cos
 from numpy import array
 
 if __name__ == "__main__":
-    a = Symbol('a')
+    a = pi/4
     matrix1 = array([[sin(a),-cos(a)],[cos(a),sin(a)]])
     matrix2 = array([[cos(a),sin(a)],[-sin(a),cos(a)]])
     matrix1 = matrix1 * sin(a)
