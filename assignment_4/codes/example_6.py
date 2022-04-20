6b1bcf767bb8560880c4b25c8efb42fe0a576627
D-Chandrahas <97086964+D-Chandrahas@users.noreply.github.com>
2022-04-20 17:14:37 +0530

    Add files via upload

---
+++ assignment_4/codes/example_6.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+if __name__ == "__main__":
+
+    freq = np.random.uniform(low=0.0, high=20000.0, size=1000)
+    
+    class1 = 0
+    class2 = 0
+    class3 = 0
+    class4 = 0
+
+    for i in freq:
+        if i < 4000:
+            class1 += 1
+        elif i >= 4000 and i <= 9000:
+            class2 += 1
+        elif i > 9000 and i <=14000:
+            class3 += 1
+        else:
+            class4 += 1
+        
+    print("Distance        Frequency\n< 4000          %.0f\n4000 - 9000     %.0f\n9001 - 14000    %.0f\n> 14000         %.0f\n" % (class1,class2,class3,class4))
+    
+    
