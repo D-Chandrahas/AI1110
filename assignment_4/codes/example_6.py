2cf3dd5fc749d6d2ef811783bab27d668926419c
D-Chandrahas <97086964+D-Chandrahas@users.noreply.github.com>
2022-04-23 15:24:50 +0530

    Add files via upload

--- assignment_4/codes/example_6.py
+++ assignment_4/codes/example_6.py
@@ -10,15 +10,17 @@ if __name__ == "__main__":
     class3 = 0
     class4 = 0
 
-    for i in freq:
-        if i < 4000:
+    i=0
+    while i < 1000:
+        if freq[i] < 4000:
             class1 += 1
-        elif i >= 4000 and i <= 9000:
+        elif freq[i] >= 4000 and freq[i] <= 9000:
             class2 += 1
-        elif i > 9000 and i <=14000:
+        elif freq[i] > 9000 and freq[i] <=14000:
             class3 += 1
         else:
             class4 += 1
+        i += 1
         
     print("Distance        Frequency\n< 4000          %.0f\n4000 - 9000     %.0f\n9001 - 14000    %.0f\n> 14000         %.0f\n" % (class1,class2,class3,class4))
     
