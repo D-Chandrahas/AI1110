b466af5a82c7a39e046339e05a21d3623fc54cff
D-Chandrahas <97086964+D-Chandrahas@users.noreply.github.com>
2022-04-27 10:28:19 +0530

    Add files via upload

--- assignment_4/codes/example_6.py
+++ assignment_4/codes/example_6.py
@@ -5,11 +5,21 @@ if __name__ == "__main__":
 
     freq = np.random.uniform(low=0.0, high=20000.0, size=1000)
     
-    class1 = [f for f in freq if f < 4000]
-    class2 = [f for f in freq if f >= 4000 and f <= 9000]
-    class3 = [f for f in freq if f > 9000 and f <= 14000]
-    class4 = [f for f in freq if f > 14000]
+    class1 = 0
+    class2 = 0
+    class3 = 0
+    class4 = 0
+    
+    for i in freq:
+        if i < 4000:
+            class1 += 1
+        elif i >= 4000 and i <= 9000:
+            class2 += 1
+        elif i > 9000 and i <=14000:
+            class3 += 1
+        else:
+            class4 += 1
         
-    print("Distance        Frequency\n< 4000          %.0f\n4000 - 9000     %.0f\n9001 - 14000    %.0f\n> 14000         %.0f\n\n" % (len(class1),len(class2),len(class3),len(class4)))
+    print("Distance        Frequency\n< 4000          %.0f\n4000 - 9000     %.0f\n9001 - 14000    %.0f\n> 14000         %.0f\n\n" % (class1,class2,class3,class4))
     
     
